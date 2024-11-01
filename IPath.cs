8d9683985a214e9fadcecbdcdc2d4ca1fc32c5e1
Roger Crawfis <roger.crawfis@gmail.com>
2024-11-01 11:23:34 -0400

    Moved the Occupancy Grid conversion to a new class MazeOccupancyGrids. Moved the StampSet creation methods from the Maze project to a new class, StampSetFactory in the Grid project. Renamed MazeBuilderWeaver to MazeBuilderBraids Renamed Collections.EnumerableHelpers to EnumerableExtensions and moved extensions from PCGGraph to this class. Clean-up New documentation on all of the DLL's

--- IPath.cs
+++ IPath.cs
@@ -7,16 +7,16 @@ namespace CrawfisSoftware.Collections.Path
     /// </summary>
     /// <typeparam name="TPosition">The type of the position (e.g., Vector3, (i,j)-tuple, int, etc.)</typeparam>
     /// <typeparam name="TEdgeValue">The type of the "distance" (or time) of the path.</typeparam>
-    public interface IPath<TPosition,TEdgeValue> : IReadOnlyList<TPosition>
+    public interface IPath<TPosition, TEdgeValue> : IReadOnlyList<TPosition>
     {
         /// <summary>
         /// If true, the path forms a loop connecting the last position to the first.
         /// </summary>
         public bool IsClosed { get; }
         /// <summary>
-        /// The number of positions that are defined on this path.
+        /// The number of positions that are defined on this path. If it is a loop then the count is one more than the number of positions.
         /// </summary>
-        public TPosition PositionCount { get; }
+        public int PositionCount { get; }
         /// <summary>
         /// The path length computed by some measure.
         /// </summary>
